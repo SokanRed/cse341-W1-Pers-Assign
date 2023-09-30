428c10f60112e88226cc4e1737319278fce20fac
Denis PC Poussard <denis@sokan.fr>
2023-09-30 21:24:16 +0200

    Change the PUT function in controllers contact

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,6 +1,6 @@
 const contact = require('../db/connect');
-//const { ObjectId } = require('mongodb');
-const ObjectId = require('mongodb').ObjectId
+const { ObjectId } = require('mongodb');
+// const ObjectId = require('mongodb').ObjectId
 
 const getContact = async(req, res) => {
     const Db = await contact.getDb();
@@ -41,19 +41,39 @@ const postContact = async(req, res) => {
     // }
 };
 
+// const putContactById = async(req, res) => {
+//     const userId = new ObjectId(req.params.id);
+
+//     const body = {
+//         "firstName": "Francis",
+//         "lastName": "Spagnolini",
+//         "email": "francisspagnolini@gmail.com",
+//         "favoriteColor": "Turquoise",
+//         "birthday": "1956/09/26"
+//     };
+//     const updateContactById = await contact.putContactById(userId, body);
+//     res.status(204).send("Contact was updated");
+// };
+
 const putContactById = async(req, res) => {
     const userId = new ObjectId(req.params.id);
-
     const body = {
-        "firstName": "Francis",
-        "lastName": "Spagnolini",
-        "email": "francisspagnolini@gmail.com",
-        "favoriteColor": "Turquoise",
-        "birthday": "1956/09/26"
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
     };
-    const updateContactById = await contact.putContactById(userId, body);
-    res.status(204).send("contact was updated");
-};
+    const response = await contact.putContactById(userId, body);
+    res.status(204).send("Contact was updated");
+    // console.log(response);
+    // if (response.modifiedCount > 0) {
+    //     res.status(204).send(`Contact Updated`);
+    // } else {
+    //     res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    // }
+
+}
 
 const deleteContactById = async(req, res) => {
     const userId = new ObjectId(req.params.id);
